932e303e977d1739179749fc836ebbf11f8d0f3f
Shane Walsh <walsh.shane@icloud.com>
2018-10-16 21:53:06 +0100

    Initial Commit

---
+++ RSR-Pechhulp-Clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSR-Pechhulp-Clone
+//
+//  Created by Shane Walsh on 16/10/2018.
+//  Copyright © 2018 Shane Walsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RSR-Pechhulp-CloneTests/RSR_Pechhulp_CloneTests.swift
@@ -0,0 +1,36 @@
+//
+//  RSR_Pechhulp_CloneTests.swift
+//  RSR-Pechhulp-CloneTests
+//
+//  Created by Shane Walsh on 16/10/2018.
+//  Copyright © 2018 Shane Walsh. All rights reserved.
+//
+
+import XCTest
+@testable import RSR_Pechhulp_Clone
+
+class RSR_Pechhulp_CloneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
